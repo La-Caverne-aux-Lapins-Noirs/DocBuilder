37d99837ddfbc32aa43020980abd075e80772ee6
Jason Brillante <damdoshi@hotmail.com>
2021-09-15 10:00:28 +0200

    Progression.

--- src/tools/GetGlobalMedals.php
+++ src/tools/GetGlobalMedals.php
@@ -1,24 +1,33 @@
 <?php
 require_once (__DIR__."/RetrieveMedal.php");
 
-function GetGlobalMedals()
+function GetLocalMedals($array)
 {
-    global $DocBuilder;
-
-    $Medals = [];
-    // On fait la somme des médailles de tous les exercices
-    foreach ($DocBuilder->Activity["Exercises"] as $ex)
+    $out = [];
+    foreach ($array as $ex)
     {
 	if (isset($ex["Medals"]))
 	{
 	    foreach ($ex["Medals"] as $med)
 	    {
-		if (!isset($Medals[$med]))
-		    $Medals[$med] = 0;
-		$Medals[$med] += 1;
+		if (!isset($out[$med]))
+		    $out[$med] = 0;
+		$out[$med] += 1;
 	    }
 	}
+	else if (IsArray($ex))
+	    $out = array_merge($out, GetLocalMedals($ex));
     }
+    return ($out);
+}
+
+function GetGlobalMedals()
+{
+    global $DocBuilder;
+
+    // On fait la somme des médailles de tous les exercices
+    $Medals = GetLocalMedals($DocBuilder->Activity["Exercises"]);
+
     // On ajoute les médailles de l'exercice complet
     if (isset($DocBuilder->Activity["Medals"]))
     {
