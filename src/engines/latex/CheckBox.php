df745742e30eb8bb973efda33cd1ffb1402b50a5
Damdoshi <damdoshi@hotmail.com>
2026-07-29 18:04:43 +0200

    Checkbox.

--- src/engines/latex/CheckBox.php
+++ src/engines/latex/CheckBox.php
@@ -2,8 +2,6 @@
 
 function	CheckBox($rules)
 {
-    if (@$rules[2])
-	print_r($rules);
     if (isset($rules[1]) && $rules[1] != "")
 	return ("\\checkedbox\\nobreakspace");
     return ("\\checkbox\\nobreakspace");
